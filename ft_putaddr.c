942045d0d64a09902c69860cce1f6840e8f661cf
Ahmed Ait Elaaoud <aait-ela@c2r6p9.1337.ma>
2025-11-13 15:03:11 +0100

    ready to push

---
+++ ft_putaddr.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putaddr.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: aait-ela <aait-ela@student.1337.ma>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/11/13 14:39:58 by aait-ela          #+#    #+#             */
+/*   Updated: 2025/11/13 14:43:11 by aait-ela         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+static void	ft_puthexa_ul(unsigned long n, size_t *count)
+{
+	char	*base;
+
+	base = "0123456789abcdef";
+	if (n >= 16)
+		ft_puthexa_ul(n / 16, count);
+	ft_putchar(base[n % 16], count);
+}
+
+void	ft_putaddr(void *ptr, size_t *count)
+{
+	unsigned long	addr;
+
+	addr = (unsigned long)ptr;
+	if (!ptr)
+	{
+		*count += write(1, "(nil)", 5);
+		return ;
+	}
+	*count += write(1, "0x", 2);
+	ft_puthexa_ul(addr, count);
+}
