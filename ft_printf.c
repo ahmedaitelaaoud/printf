9131bede9759738e85b0d465e6b13f0fb9e7bd81
Ahmed Ait Elaaoud <aait-ela@c3r7p4.1337.ma>
2025-11-13 14:26:07 +0100

    ready to push

--- ft_printf.c
+++ ft_printf.c
@@ -12,4 +12,45 @@
 
 #include "ft_printf.h"
 
+static void	handle_conversion(char spec, va_list args, size_t *count)
+{
+	if (spec == 'c')
+		ft_putchar(va_arg(args, int), count);
+	else if (spec == 's')
+		ft_putstr(va_arg(args, char *), count);
+	else if (spec == 'd' || spec == 'i')
+		ft_putnbr(va_arg(args, int), count);
+	else if (spec == 'p')
+		ft_putaddr(va_arg(args, void *), count);
+	else if (spec == 'u')
+		ft_putunbr(va_arg(args, unsigned int), count);
+	else if (spec == 'x' || spec == 'X')
+		ft_puthexa(va_arg(args, unsigned int), count, spec);
+	else if (spec == '%')
+		ft_putchar('%', count);
+}
 
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+	size_t	count;
+	size_t	i;
+
+	va_start(args, format);
+	count = 0;
+	i = 0;
+	while (format[i])
+	{
+		if (format[i] == '%')
+		{
+			i++;
+			if (format[i])
+				handle_conversion(format[i], args, &count);
+		}
+		else
+			ft_putchar(format[i], &count);
+		i++;
+	}
+	va_end(args);
+	return ((int)count);
+}
